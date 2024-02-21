78019f37a714d16b0c6a5045b0110752b0283ac4
Hayes Chiasson <hayeschiasson1@yahoo.com>
2024-02-21 14:04:23 -0600

    Finish Prog 1.4 Game

--- Prog1.4/sketch.js
+++ Prog1.4/sketch.js
@@ -7,69 +7,50 @@ let gameScreen;
 let timeLeft = 30;
 let gameOver = false;
 let gameEnd = false;
-let isSquished = false;
 let spawnRate = 10;
-let bugsSquished = 0;
-let bugsSquashed;
-
-let startOrientation = 0;
-
-
+let bugsSquished = 0; // score
+let startOrientation = [0, 90, 180, 270];
+let startSpawn = false;
+let speed = 1;
+let restartKey = 'r';
 
 function preload() {
 
+  spriteSheet = loadImage("assets/Bug.Png");
   gameFont = loadFont("assets/PressStart2P-Regular.ttf"); // load in google font
 
   //row: index row (vert) column: index colmn (horiz) frames: no index to go through
   animations = {
-    idleUp: {row: 0, frames: 1},
-    goUp: {row: 0, col: 1, frames: 2},
-    squishUp: {row: 3, col: 3, frames: 1},
-    goRight: {row: 1, col: 1, frames: 2},
-    squishRight: {row:1, col: 3, frames: 1},
-    goLeft: {row: 2, col: 1, frames: 2},
-    squishLeft: {row: 2, col: 3, frames: 1},
-    goDown: {row: 3, col: 1, frames: 2},
-    squishDown: {row: 3, col: 3, frames: 1}
+    idle: {row: 0, frames: 1},
+    walk: {row: 0, col: 1, frames: 2},
+    squish: {row: 3, col: 3, frames: 1},
   };
-  startOrientation = Math.round(random(1,4));
-
-    for(let i = 0; i < 30; i++) 
-    {
-      bugs.push(new Bug(random(50, 968), random(50, 968), 32, 32, "assets/Bug.png", animations));
-    }
 
 }
-
 function setup() {
   
   createCanvas(1000, 1000);
   textFont(gameFont); 
-  bugsSquashed = 0;
   gameScreen = 'start'; // figure this out
   
 }
-
 function draw() {
   background(160,82,45);
+  if (gameScreen === 'start')
+  { 
+    startMenu();
+  }
+  else if (gameScreen === 'endScreen')
+  {
+    endScreen();
+  }
+  else if (gameScreen === 'playing')
+  {
+    playing();
+  }
 
-if (gameScreen === 'start')
-{
-  startMenu();
 }
-else if (gameScreen === 'playing')
-{
-  playing();
-}
-// else if (gameScreen === 'endScreen');
-// {
-//   endScreen();
-// }
-
-}
-
 class Bug {
-
   constructor (x, y, width, height, spriteSheet, animations) {
     this.sprite = new Sprite(x, y, width, height); // add param
     this.sprite.spriteSheet= spriteSheet; // add param
@@ -77,53 +58,42 @@ class Bug {
     this.sprite.collider = 'none';
     this.sprite.anis.frameDelay = 8;//how many frames to wait before going to next frame / sets speed
     this.sprite.addAnis(animations); //add the animations / movements selected by movements in preload()
-    this.sprite.changeAni("idleUp"); //select ^^ animation to display
+    this.sprite.changeAni("idle"); //select ^^ animation to display
+    this.isSquished  = false;
+    
     }
-
      //implementation of walking animations / speed
-      goUp() {
-      this.sprite.changeAni("goUp"); // no need for rotation
-      this.sprite.vel.y = -1; //.y means y value/ -1 means subtract y so up
+    goUp() {
+      this.sprite.changeAni("walk"); // no need for rotation
+      this.sprite.vel.y = -speed; //.y means y value/ -1 means subtract y so up
       this.sprite.vel.x = 0;
-
-      }
+      this.sprite.rotation = 0;
+    }
         
     goRight() {
-      this.sprite.changeAni("goRight")
-      this.sprite.vel.x = 1; // makes character walk forward/ off screen to right
-      this.sprite.scale.x = 1; // right facing
+      this.sprite.changeAni("walk")
+      this.sprite.vel.x = speed; // makes character walk forward/ off screen to right
       this.sprite.vel.y = 0; // make unused axis to 0 / makes them stop
+      this.sprite.rotation = 90;
     }
         
     goLeft() {
-    this.sprite.changeAni("goRight"); // same ani, just switch scale
-    this.sprite.vel.x = -1 // go left
-    this.sprite.scale.x = -1;//same size, flip horizontally 
+    this.sprite.changeAni("walk"); // same ani, just switch scale
+    this.sprite.vel.x = -speed // go left
     this.sprite.vel.y = 0; //unused axis
+    this.sprite.rotation = 270;
   }
-
   goDown() {
-    this.sprite.changeAni("goDown");
-    this.sprite.vel.y = 1; // cdown bc increasing in y
+    this.sprite.changeAni("walk");
+    this.sprite.vel.y = speed; // cdown bc increasing in y
     this.sprite.vel.x = 0;
+    this.sprite.rotation = 180;
   }
   squish() {
-    if (startOrientation == 1)
-    {
-    this.sprite.changeAni("squishRight")
-    }
-    if (startOrientation == 2)
-    {
-    this.sprite.changeAni("squishLeft")
-    }
-    if (startOrientation == 3)
-    {
-    this.sprite.changeAni("squishUp")
-    }
-    if (startOrientation == 4)
-    {
-    this.sprite.changeAni("squishDown")
-    }
+    this.sprite.changeAni("squish");
+    this.sprite.vel.x = 0;
+    this.sprite.vel.y = 0;
+    this.isSquished = true;
   }
 
   contains(x,y) {
@@ -131,116 +101,135 @@ class Bug {
     let insideY = y >= this.sprite.y && y <= this.sprite.y + 32;
    return insideX && insideY;
   }
-
-  bounds() {
-  
-  if (sprite.x  + sprite.width/4 > width) // if sprite x is past edge of canvas(width)/ for right side sprite.width /2 gives padding
-  { 
-    walkLeft();
-  }
-  else if (sprite.x - sprite.width/4 < 0) //if touches left of canvas / x = 0 / padding spritelength /4
-  {
-    walkRight();
-  }
-  else if (sprite.y + sprite.height/4  > height)
-  {
-    walkUp();
-  }
-  else if (sprite.y - sprite.height/4 < 0)
-  {
-    walkDown();
-  }
-  }
 }
 
+
+  function isBugSquished() {
+    return this.isSquished; // check for bug being squished to make sure it can't be messed with again/ after
+  }
   //display Menu text
   //get input to start
   //set playing to true
-
   function startMenu() {
     background(160,82,45);
-
+    text("Welcome to Bug Squish!", width / 3, (height / 16));
+    text("You will have 30 seconds to squish as many bugs as you can!", 100, 300);
+    text("Press space to start the Timer.", 300, 400);
     if (key === ' ') {
         gameScreen = 'playing';
+        startSpawn = true;
     }
   }
-
   function playing() {
     //game time counter
-
-    //console.log(ceil(timeLeft));
+   
     text("Time Left:" + ceil(timeLeft), 30 , 45);
     text("Bugs Squashed: " + bugsSquished , width - 210 , 45);
     timeLeft -= deltaTime / 1000; //track time (deltaTime) convert from milli -> seconds (/1000)
-
-    if (timeLeft == 0)
+    if (timeLeft <= 0)
     {
       gameScreen = 'endScreen';
+      timeLeft = 0;
     }
+
+    if (startSpawn == true) // check if spawning enabled
+    {
+      startSpawn = false; // if spawning, disable it until next space click / game start
+
+    for(let i = 0; i < 55; i++) //bug spawner
+    {
+      bugs.push(new Bug(random(50, 968), random(50, 968), 32, 32, spriteSheet, animations));
 
     bugs.forEach((bug) => {
 
-      if (startOrientation == 1)
+      bug.rotation = random(startOrientation);
+
+      if (bug.rotation === 90)
       {
         bug.goRight();
       }
-      else if (startOrientation == 2)
+      else if (bug.rotation === 270)
       {
         bug.goLeft();
       }
-      else if (startOrientation == 3)
+      else if (bug.rotation === 0)
       {
         bug.goUp();
       }
-      else if (startOrientation == 4)
+      else if (bug.rotation === 180)
       {
         bug.goDown();
       }
-        mouseReleased(bug.squish());
-        { 
-          
-          bugsSquished++;
-          console.log("mousePressed");
-        }
-       });
-  }
+      else
+      {
+        bug.squish();
+      }
 
+       });
+      }
+    }
+    bugs.forEach((bug) => {
+      
+      if (bug.sprite.x + bug.sprite.width/4 > width) //if hit right wall
+      {
+        bug.goLeft(); // go back left
+        
+      } 
+      else if (bug.sprite.x - bug.sprite.width/4 < 0) //if hit left wall
+      {
+        bug.goRight(); // go back right
+      }
+  
+  
+      if (bug.sprite.y + bug.sprite.height/4 > height) // if reach bottom border
+      {
+        bug.goUp(); // turn around (up)
+      } 
+      else if (bug.sprite.y - bug.sprite.height/4 < 0) //if reach top border
+      {
+        bug.goDown(); // turn back down
+        
+      }
+  
+    });
+
+       //do border / bounds (make function?)
+  }
   function endScreen() {
     background(160,82,45);
     text("Good Job!", width / 3, (height / 16));
     text("Final Score: "+ bugsSquished, 100, 300);
-    text("Press space to play again.", 300, 400);
-
-    if (key === ' ') {
+    text("Press " + restartKey + " to play again.", 300, 400);
+    if (key === restartKey) {
+      if (restartKey = 'r') // added to alternate restart key between r and space, resolves error where game skips straight to end scene
+      {
+        restartKey = ' ';
+      }
+      else
+      {
+        restartKey = 'r';
+      }
+        //reset/ restart gane
         gameScreen = 'playing';
+        startSpawn = true;
+        speed = 1;
+        bugsSquished = 0;
+        timeLeft = 30;
     }
+  }
+  function mouseReleased() { // detect press on bug / stop dragging
+
+    bugs.forEach((bug) => {
+      if (!bug.isSquished && gameScreen === 'playing') // check if game is playing & the bug isn't already squished
+      {
+        if (bug.contains(mouseX, mouseY)) // if mouse is inside bug
+        {
+          bug.squish(); //squish it
+          bugsSquished++; // and to counter
+          speed += 0.1; // increase speed
+        }
+      }
+      
+    })
   }
 
-  function mouseReleased()
-  {
-    for (let i = 0; i < animations.length; i++) {
-    if (bugs[0].contains(mouseX, mouseY))
-    { 
-      if (startOrientation == 1)
-      {
-      sprite.changeAni("squishRight")
-      }
-      if (startOrientation == 2)
-      {
-      sprite.changeAni("squishLeft")
-      }
-      if (startOrientation == 3)
-      {
-      sprite.changeAni("squishUp")
-      }
-      if (startOrientation == 4)
-      {
-      sprite.changeAni("squishDown")
-      }
-      sprite.vel.x = 0;
-      sprite.vel.y = 0;
-      bugsSquished++;
-      console.log("mousePressed");
-    }
-  }
-}

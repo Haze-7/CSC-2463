aaa7d1b4aa6c73694528d3b46144b34f001eddd0
Hayes Chiasson <hayeschiasson1@yahoo.com>
2024-02-23 16:55:01 -0600

    Prog 2.1 Progress

--- Class09/sketch.js
+++ Class09/sketch.js
@@ -1,10 +1,12 @@
 let sounds = new Tone.Players ({
 
-  'piano': "assets/piano.mp3"
+  'piano': "assets/piano.mp3",
+  'meditation': "assets/meditation.mp3",
+  
 });
 
 let delAmt = new Tone.FeedbackDelay("8n", 0.5); // set speed (8n (8th note), how much delay(0.5) / distortion ammount
-let disAmt = new Tone.Distortion(0.5); //distortion amount
+let distAmt = new Tone.Distortion(0.5); //distortion amount
 let button1, button2;
 let delaySlider;
 let fbSlider; //feedback slider
@@ -19,13 +21,13 @@ sounds.toDestination(); //breaks up sound source to add effects before sending t
 function setup() {
   createCanvas(400, 400);
 
-  button1 = createButton('Popcorn Maker');
+  button1 = createButton('Piano');
   button1.position(85, 150);
-  button1.mousePressed( () => sounds.player('popcorn').start());
+  button1.mousePressed( () => sounds.player('piano').start());
   
-  button2 = createButton('Piano Player');
+  button2 = createButton('Meditation');
   button2.position(205, 150);
-  button2.mousePressed( () => sounds.player('piano').start());
+  button2.mousePressed( () => sounds.player("meditation").start());
 
   delaySlider = createSlider(0, 1, 0, 0.05); // range 0(none) to max(1), start @ 0, gradual 0.05
   delaySlider.position(120, 200);
--- Prog2.1/sketch.js
+++ Prog2.1/sketch.js
@@ -1,30 +1,69 @@
 //set up sounds
 let sounds = new Tone.Players ({
 
-  'piano': "assets/piano.mp3"
-  //add 3 more 
+  'piano': "assets/piano.mp3",
+  'guitar': "assets/guitar.mp3",
+  'drums': "assets/drums.mp3",
+  'trombone': "assets/trombone.mp3"
 });
 
 //set up buttons to select sound
-let button1, button2;
+let pianoButton;
+let guitarButton;
+let drumsButton;
+let tromboneButton;
+
+let pitchLevel = new Tone.PitchShift(-12, 7);
+let reverbLevel = new Tone.Reverb();
+
+let pitchSlider; 
+let reverbSlider;
 
 //variables for effects
 
+sounds.connect(pitchLevel);
+pitchLevel.toDestination(reverbLevel);
+sounds.toDestination();
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 
-  button2 = createButton('Piano Player');
-  button2.position(85, 150);
-  button2.mousePressed( () => sounds.player('piano').start());
+  pianoButton = createButton('Piano');
+  pianoButton.position(25, 50);
+  pianoButton.mousePressed( () => sounds.player
+  ('piano').start());
+
+  guitarButton = createButton('Guitar');
+  guitarButton.position(150, 50);
+  guitarButton.mousePressed(() => sounds.player
+  ('guitar').start());
+
+  drumsButton = createButton("Drums");
+  drumsButton.position(275, 50);
+  drumsButton.mousePressed(() => sounds.player
+  ('drums').start());
+
+  tromboneButton = createButton("Trombone");
+  tromboneButton.position(400, 50);
+  tromboneButton.mousePressed(() => sounds.player
+  ('trombone').start());
 
   // add an effect
 
   //add slider / button to make it work
+
+  pitchSlider = createSlider (-12, 7, 0, 0.05);
+  pitchSlider.position(25, 150);
+  pitchSlider.mouseMoved(() => pitchSlider.feedback.value = pitchSlider.value());
 }
 
 function draw() {
   background(220);
+
+  textSize(20);
+  text("Welcome to Music Player!", 25, 25);
+  text("Welcome to Music Player!");
+
 
   //create labeling text to explain how to use the sampler
   //text();
